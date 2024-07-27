379183c67b36de41260b204ac4a1ea416d22ed0a
M KAILASNATH <mkailasnath@Ms-MacBook-Air.local>
2024-07-27 13:50:50 +0530

    string encryption over

--- lab_cycle_1/string_encryption.R
+++ lab_cycle_1/string_encryption.R
@@ -1,6 +1,31 @@
-sentence <- readline(prompt="enter the sentence: ")
+caesar_cipher_encrypt <- function(sentence, shift) {
+  
+  shift_char <- function(char, shift) {
+    if (grepl("[a-z]", char)) {
+      new_char <- intToUtf8(((utf8ToInt(char) - utf8ToInt("a") + shift) %% 26) + utf8ToInt("a"))
+    } else if (grepl("[A-Z]", char)) {
+      new_char <- intToUtf8(((utf8ToInt(char) - utf8ToInt("A") + shift) %% 26) + utf8ToInt("A"))
+    } else {
+      new_char <- char
+    }
+    return(new_char)
+  }
+  
+  encrypted_sentence <- paste(sapply(strsplit(sentence, NULL)[[1]], shift_char, shift), collapse = "")
+  return(encrypted_sentence)
+  
+}
 
 
+sentence <- readline(prompt = "Enter a sentence to encrypt: ")
+shift <- as.integer(readline(prompt = "Enter shift value: "))
 
+
+if (is.na(shift) || shift < 0) {
+  cat("Invalid shift value. Please enter a non-negative integer.\n")
+} else {
+  encrypted_sentence <- caesar_cipher_encrypt(sentence, shift)
+  cat("Encrypted sentence:", encrypted_sentence, "\n")
+}
 
 
