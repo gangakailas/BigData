5ce2a869700ade6650d937b1e9d5c48c6d679594
Ganga Kailas <gangakailasnath@gmail.com>
2024-08-26 22:44:24 +0530

    histogram mpg

---
+++ lab_cycle_2/histogram_mpg/histo_mpg.R
@@ -0,0 +1,28 @@
+library(ggplot2)
+
+ggplot(mtcars, aes(x=mpg))+
+  geom_histogram(binwidth = 2,
+                 aes(fill = after_stat(count)),
+                 color = "black",
+                 alpha = 0.7)+
+  scale_fill_gradient(low ="lightblue", high = "darkblue")+
+  labs(title = "Mile per Miles per Gallons(mpg)",
+       x = "Miles per Gallon",
+       y = "Frequency")+
+  theme_minimal()+
+  geom_vline(aes(xintercept = mean(mpg)),
+             color = "red", linetype = "dashed", size = 1)+
+  annotate("text", x = mean(mtcars$mpg), y = 6,
+           label = paste("Mean", round(mean(mtcars$mpg), 2)),
+           color = "red")+
+  geom_vline(aes(xintercept = mean(mpg) - sd(mpg)),
+             color = "green", linetype = "dotted", size = 1)+
+  geom_vline(aes(xintercept = mean(mpg) + sd(mpg)),
+             color = "green", linetype = "dotted", size = 1)+
+  annotate("text", x = mean(mtcars$mpg) - sd(mtcars$mpg), y = 5.5,
+           label = paste("Mean - SD", round(mean(mtcars$mpg) - sd(mtcars$mpg), 2)),
+           color = "green")+
+  annotate("text", x = mean(mtcars$mpg) + sd(mtcars$mpg), y = 5.5,
+           label = paste("Mean + SD", round(mean(mtcars$mpg) + sd(mtcars$mpg), 2)),
+           color = "green")
+  
