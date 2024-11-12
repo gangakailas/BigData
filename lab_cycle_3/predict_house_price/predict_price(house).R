1ce5b6ef062ba8dae89acd1e315545c53286185f
Ganga Kailas <gangakailasnath@gmail.com>
2024-11-13 00:42:38 +0100

    cycle 3 done

---
+++ lab_cycle_3/predict_house_price/predict_price(house).R
@@ -0,0 +1,20 @@
+library(ggplot2)
+
+path <- '/Users/mkailasnath/Documents/R/train.csv'
+data <- read.csv(path)
+
+model <- lm(SalePrice ~ GrLivArea, data = data)
+
+slope <- coef(model)[2]
+intercept <- coef(model)[1]
+
+cat("Slope:", slope, "\n")
+cat("Intercept:", intercept, "\n")
+
+ggplot(data, aes(x = GrLivArea, y = SalePrice)) +
+  geom_point(alpha = 0.5) +  # Scatter plot of the data points
+  geom_abline(slope = slope, intercept = intercept, col = 'olivedrab', linewidth = 1) +
+  labs(title = "Linear Regression of House Prices vs Size",
+       x = "Size (GrLivArea)",
+       y = "Price (SalePrice)") +
+  theme_minimal()
